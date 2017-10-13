2b851b5213c8164a671ac9733506626856f3df43
Nicholas Shindler <shindler25@gmail.com>
2017-10-13 16:27:35 -0700

    async test

--- koa-simple-server/server.js
+++ koa-simple-server/server.js
@@ -1,10 +1,24 @@
 const Koa = require('koa');
+const _ = require('koa-route');
+
 const app = new Koa();
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+const test = {
+  test: (ctx) => {
+    // response
+    ctx.body = 'Hello Koa';
+  },
+
+  testRoute: async (ctx, message, next) => {
+    await next();
+    ctx.response.body = `I like ${message}`;
+  },
+
+}
+
+app.use(_.get('/', test.test));
+app.use(_.get('/test/:message', test.testRoute));
+
 
 
 
