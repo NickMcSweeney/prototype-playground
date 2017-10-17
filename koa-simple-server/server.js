d76df50631639d588b23ce671cbe05d2b363a0ab
Nicholas Shindler <shindler25@gmail.com>
2017-10-16 18:14:44 -0700

    translate api

--- koa-simple-server/server.js
+++ koa-simple-server/server.js
@@ -1,22 +1,24 @@
 const Koa = require("koa");
 const Router = require("koa-router");
-const IO = require("koa-socket");
 const translate = require("google-translate-api");
+const cors = require('koa-cors');
+const bodyParser = require("koa-bodyparser");
 
 const app = new Koa();
 const test = new Router();
-const io = new IO();
 
 let data = "poodles of rain";
 
 const strings = {
-  BaBaBlackSheep: "Schaapje, schaapje, heb je witte wol? \nJa baas, ja baas, drie zakken vol \nEén voor de meester en één voor z'n vrouw \nEén voor het kindje dat bibbert van de kou \nSchaapje, schaapje, heb je witte wol? \nJa baas, ja baas, drie zakken vol.",
-  PoesjeMauw: "Poesje Mauw \nKom eens gauw \nIk heb lekkere melk voor jou \nEn voor mij \nRijstebrij \nO wat heerlijk smullen wij!"
-}
+  BaBaBlackSheep:
+    "Schaapje, schaapje, heb je witte wol? \nJa baas, ja baas, drie zakken vol \nEén voor de meester en één voor z'n vrouw \nEén voor het kindje dat bibbert van de kou \nSchaapje, schaapje, heb je witte wol? \nJa baas, ja baas, drie zakken vol.",
+  PoesjeMauw:
+    "Poesje Mauw \nKom eens gauw \nIk heb lekkere melk voor jou \nEn voor mij \nRijstebrij \nO wat heerlijk smullen wij!",
+};
 
 const myTestRoutes = {
   hello: ctx => {
-    ctx.body = "Hello Koa";
+    ctx.body = "Hello World";
   },
 
   testRoute: async (ctx, next) => {
@@ -26,22 +28,53 @@ const myTestRoutes = {
 
   translateStr: async (ctx, next) => {
     try {
-      let res = await translate(
-        strings[ctx.params.str],
-        { from: "nl", to: "en" }
-       )
+      console.log(ctx.request.body);
+      let res = await translate(strings[ctx.params.str], {
+        from: "nl",
+        to: "en",
+      });
       console.log("Success", res.text);
-      ctx.response.body = res.text;
+      ctx.response.body = `NL: ${strings[
+        ctx.params.str
+      ]}\n -- TO -- \nEN: ${res.text}`;
     } catch (e) {
       console.error(e);
     } finally {
-
     }
   },
 
   inputTest: async (ctx, next) => {
-    await next();
-    ctx.response.body = `I hate ${data}`;
+    try {
+      console.log("Testing input", ctx.request);
+      await next();
+      ctx.body = ctx.request.body;
+      console.log("Your request is", ctx.body, ctx.request.body);
+
+      // ctx.response.body = "Sucess - ish";
+      ctx.response.status = 200;
+      ctx.response.type = "application/json";
+      ctx.response.body = {text: `I hate ${ctx.body.text}`};
+      console.log("Finished input test", ctx.response);
+      return ctx.response;
+    } catch (e) {}
+  },
+
+  inputTranslate: async (ctx, next) => {
+    try {
+      console.log("Testing input", ctx.request);
+      ctx.body = ctx.request.body;
+      let res = await translate(ctx.body.text, {
+        from: ctx.body.langOrigin,
+        to: ctx.body.langOut,
+      });
+
+      // ctx.response.body = "Sucess - ish";
+      ctx.response.status = 200;
+      ctx.response.type = "application/json";
+      ctx.response.body = res;
+      console.log("Finished input test", ctx.response);
+      return ctx.response;
+    } catch (e) {}
   },
 };
 
@@ -50,18 +83,12 @@ console.log("Starting server at localhost:3001 . . . \n");
 test.get("/test/:message", myTestRoutes.testRoute);
 test.get("/translate/:str", myTestRoutes.translateStr);
 test.get("/test/", myTestRoutes.hello);
-test.get("/input/", myTestRoutes.inputTest);
+test.post("/input/", myTestRoutes.inputTest);
+test.post("/input/translate/", myTestRoutes.inputTranslate);
 
-io.attach(app);
-
+app.use(cors());
+app.use(bodyParser());
 app.use(test.routes());
 
-// app.io.on('/input/', async (ctx, next) => {
-//   data = "rain poodles"
-//   await next();
-//   console.log(data, 'are wet');
-// });
-
 app.listen(3001);
-app.server.listen(process.env.PORT || 3001);
 console.log("server running");
